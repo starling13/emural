8389d086db8fc804543476e90219f50bd6d5f2b4
Andrey Skvortsov <starling13@mail.ru>
2026-05-05 02:19:32 +0300

    Update visual

--- emural_3d_pult/main.gd
+++ emural_3d_pult/main.gd
@@ -15,11 +15,11 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		var e: InputEventMouseMotion = event as InputEventMouseMotion
 		if e.button_mask & BUTTON_RIGHT:
-			$Camera.rotation.y += e.relative.x/250.0
-			$Camera.rotation.x += e.relative.y/250.0
+			$ural/Camera.rotation.y += e.relative.x/250.0
+			$ural/Camera.rotation.x += e.relative.y/250.0
 	elif event is InputEventMouseButton:
 		var e: InputEventMouseButton = event as InputEventMouseButton
 		if e.button_index == BUTTON_WHEEL_UP:
-			$Camera.fov = max(20.0, $Camera.fov-1)
+			$ural/Camera.fov = max(20.0, $ural/Camera.fov-1)
 		elif e.button_index == BUTTON_WHEEL_DOWN:
-			$Camera.fov = min(90.0, $Camera.fov+1)
+			$ural/Camera.fov = min(90.0, $ural/Camera.fov+1)
