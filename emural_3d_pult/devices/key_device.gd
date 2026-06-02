c06e45cfbb3afa25507f0ddee3a93e37a43b15f1
Andrey Skvortsov <starling13@mail.ru>
2026-06-03 01:24:20 +0300

    Add keys to keydev. Update ecvm cabinet model

---
+++ emural_3d_pult/devices/key_device.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+
+func _ready():
+	$Device/buttons_0.set_colors(Color.white, Color.black)
+	$Device/buttons_1.set_colors(Color.white, Color.black)
+	$Device/buttons_2.set_colors(Color.white, Color.black)
+	$Device/buttons_3.set_colors(Color.black, Color.white)
+	$Device/buttons_4.set_colors(Color.black, Color.white)
+	$Device/buttons_5.set_colors(Color.black, Color.white)
+	$Device/buttons_6.set_colors(Color.black, Color.white)
+	$Device/buttons_7.set_colors(Color.black, Color.white)
+	$Device/buttons_8.set_colors(Color.black, Color.white)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
